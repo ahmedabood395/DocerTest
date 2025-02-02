b46e769292f927d4b2019c34c432a8ae4f038a3a
Ahmed Abood <ahmedabood395>
2025-02-02 11:21:39 +0200

    commit

--- DockerTest/DockerTest/Controllers/WeatherForecastController.cs
+++ DockerTest/DockerTest/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace DockerTest.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
